2c27a8814e00d541ee046f18a581d627bedd8c69
lsh2546 <lsh2546@gmail.com>
2026-07-29 19:38:41 +0900

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+Exit code: 0
+Wall time: 0.3 seconds
+Output:
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
