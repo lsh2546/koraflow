364e5478e80cb73c1f261a102740b43cfee7f039
lsh2546 <lsh2546@gmail.com>
2026-07-29 19:38:44 +0900

    Add drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+Exit code: 0
+Wall time: 0.3 seconds
+Output:
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./db/schema.ts",
+  dialect: "sqlite",
+});
+
